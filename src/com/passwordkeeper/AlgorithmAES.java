d03bd241fb64f0d7f1c24a5d06fa19e5b5698be4
cargamoni <ahmetcanirdem@hotmail.com>
2020-08-10 10:20:37 +0100

    Todo Review

--- src/com/passwordkeeper/AlgorithmAES.java
+++ src/com/passwordkeeper/AlgorithmAES.java
@@ -1,6 +1,9 @@
 package com.passwordkeeper;
 
 import java.io.*;
+import java.nio.charset.Charset;
+import java.math.BigInteger;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
@@ -11,9 +14,52 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class AlgorithmAES
 {
+    // Yapılacaklar
+    // Parolanın MD5'li Hali Dosyada Tutulacak
+    // Bu Dosya yoksa yeni parola belirlenecek
+    // Giriş Parolanın MD5'e dönüştürülerek yapılacak ve Dosyadan Kontrol Edilcecek
+    // Encyript ve Decyript sadece Parola ile Yapılacak.
+    // Encyript edilen dosyaya yazılacak, Decyript edilen Clipboard üzerine kopyalanacak.
+
+
     public static SecretKey KeyAES = null;
     public static final String Algorithm = "AES";
+    private static String ThePassword;
     public static final String KeyFile = "files/secret.key";
+
+    public AlgorithmAES(String Password)
+    {
+        GenerateKeyAES(Password);
+    }
+
+    public String MD5HashGenerator(String Text)
+    {
+        try {
+
+            // Static getInstance method is called with hashing MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            // digest() method is called to calculate message digest
+            //  of an input digest() return array of byte
+            byte[] messageDigest = md.digest(Text.getBytes());
+
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            //System.out.println("Your Password was : " + Password + " \n Now : " + ThePassword);
+            return hashtext;
+        }
+
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public File CreateFile(String FileName)
     {
@@ -46,15 +92,15 @@ public class AlgorithmAES
         return OutFile.toString();
     }
 
-    public boolean AreKeysPresent()
-    {
-        File DesKey = new File(KeyFile);
-        if(DesKey.exists())
-            return true;
-        return false;
-    }
+//    public boolean AreKeysPresent()
+//    {
+//        File DesKey = new File(KeyFile);
+//        if(DesKey.exists())
+//            return true;
+//        return false;
+//    }
 
-    public void GenerateKeyAES()
+    public SecretKey GenerateKeyAES(String Password)
     {
         File FileAES;
         KeyGenerator KeyMaker;
@@ -62,32 +108,35 @@ public class AlgorithmAES
         KeySpec spec;
         //byte [] salt = seed.generateSeed(64);
         byte [] salt = {1};
-        String password = "Asshole";
+        //String password = "Asshole";
         try {
             SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
             //spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 256);
-            spec = new PBEKeySpec(password.toCharArray(), salt, 1, 256);
+            spec = new PBEKeySpec(Password.toCharArray(), salt, 1, 256);
             SecretKey tmp = factory.generateSecret(spec);
             KeyAES = new SecretKeySpec(tmp.getEncoded(), Algorithm);
+
+            return KeyAES;
 
 //            KeyMaker = KeyGenerator.getInstance(Algorithm);
 //            KeyMaker.init(256);
 //            KeyAES = KeyMaker.generateKey();
 
-            FileAES = CreateFile(KeyFile);
-            ObjectOutputStream SecretKeyOut = new ObjectOutputStream(new FileOutputStream(FileAES));
-            SecretKeyOut.writeObject(KeyAES);
-            SecretKeyOut.close();
+//            FileAES = CreateFile(KeyFile);
+//            ObjectOutputStream SecretKeyOut = new ObjectOutputStream(new FileOutputStream(FileAES));
+//            SecretKeyOut.writeObject(KeyAES);
+//            SecretKeyOut.close();
 
-        } catch (NoSuchAlgorithmException | IOException | InvalidKeySpecException e) { e.printStackTrace();}
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) { e.printStackTrace();}
+        return null;
     }
 
     public String Encryption (String PlainText, String CipherText)
     {
         try {
-            ObjectInputStream InputStream = new ObjectInputStream(new FileInputStream(KeyFile));
-            final SecretKey KeyAES = (SecretKey) InputStream.readObject();
-            InputStream.close();
+//            ObjectInputStream InputStream = new ObjectInputStream(new FileInputStream(KeyFile));
+//            final SecretKey KeyAES = (SecretKey) InputStream.readObject();
+//            InputStream.close();
 
             Cipher CipherAES = Cipher.getInstance(Algorithm);
             CipherAES.init(Cipher.ENCRYPT_MODE,KeyAES);
@@ -116,9 +165,9 @@ public class AlgorithmAES
     public String Decryption (String CipherText, String PlainText)
     {
         try {
-            ObjectInputStream InputStream = new ObjectInputStream(new FileInputStream(KeyFile));
-            final SecretKey KeyAES = (SecretKey)InputStream.readObject();
-            InputStream.close();
+//            ObjectInputStream InputStream = new ObjectInputStream(new FileInputStream(KeyFile));
+//            final SecretKey KeyAES = (SecretKey)InputStream.readObject();
+//            InputStream.close();
 
             Cipher CipherAES = Cipher.getInstance(Algorithm);
             CipherAES.init(Cipher.DECRYPT_MODE, KeyAES);
--- src/com/passwordkeeper/Main.java
+++ src/com/passwordkeeper/Main.java
@@ -1,16 +1,20 @@
 package com.passwordkeeper;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        AlgorithmAES ObjectAES = new AlgorithmAES();
+        String EnteredPassword;
+        Scanner Pass = new Scanner(System.in);
+        AlgorithmAES ObjectAES = new AlgorithmAES(Pass.nextLine());
 
         String PlainText = ObjectAES.ReadData("files/PlainText.txt");
         System.out.println("Plain Text: " + PlainText);
 
-        if(!ObjectAES.AreKeysPresent())
-            ObjectAES.GenerateKeyAES();
+//        if(!ObjectAES.AreKeysPresent())
+//            ObjectAES.GenerateKeyAES();
 
         String Result = ObjectAES.Encryption("files/PlainText.txt", "files/CipherText.txt");
         System.out.println(Result);
